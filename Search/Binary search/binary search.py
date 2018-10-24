f928a4e1bfe6006b86596ac5a3c68e7b40913c80
jai-dewani <jai.dewani.99@gmail.com>
2018-10-24 23:58:23 +0530

    Corrected binary search.py

--- Search/Binary search/binary search.py
+++ Search/Binary search/binary search.py
@@ -1,40 +1,26 @@
-class Binary_Search
-{
-    // Function for binary search
-    public static int BinarySearch(int[] array, int size, int desired)
-    {
-        int left = 0, right = size - 1, middle;
+def binarySearch (arr, l, r, x): 
+    if r >= l: 
+        mid = l + (r - l)/2
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+        # If element is smaller than mid, then it can only be present in left subarray 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+        # Else the element can only be present in right subarray 
+        else: 
+            return binarySearch(arr, mid+1, r, x) 
+    else: 
+        # Element is not present in the array 
+        return -1
 
-        while (left <= right)
-        {
-            middle = left + (right - left) / 2;
-
-            if (array[middle] == desired)
-                return middle;
-            else if (desired < array[middle])
-                right = middle - 1;
-            else
-                left = middle + 1;
-        }
-
-        return -1;
-    }
-
-    // Driver Function
-    public static void main(String[] args)
-    {
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
-
-        // Element 4 to be searched
-        if (BinarySearch(array, 7, 4) != -1)
-            System.out.println("Found");
-        else
-            System.out.println("Not Found");
-
-        //Element 9 to be searched
-        if (BinarySearch(array, 7, 9) != -1)
-            System.out.println("Found");
-        else
-            System.out.println("Not Found");
-    }
-}
+  
+if __name__ == "__main__":        
+    arr = [ 2, 3, 4, 10, 40 ] 
+    x = 10
+    # Function call 
+    result = binarySearch(arr, 0, len(arr)-1, x) 
+    if result != -1:
+        print "Element is present at index %d" % result 
+    else: 
+        print "Element is not present"
