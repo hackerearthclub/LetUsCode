f7e2b90f029c58d2e635e9076a84f9c1d0150ac5
ashwek <ashwekswamy@protonmail.com>
2019-01-02 16:07:36 +0530

    Fix SelectionSortRecurssion.cpp

--- Sorting/SELECTION SORT/SelectionSortRecurssion.cpp
+++ Sorting/SELECTION SORT/SelectionSortRecurssion.cpp
@@ -1,16 +1,37 @@
 //Selection Sort Recursion
+
 #include<iostream>
+
 using namespace std;
-int SelectionSort(int a[],int n,int i){
-	if(i==n-1) return 
-	
+
+void SelectionSort(int a[],int s,int e){
+
+	if(s==e-1) return;
+
+	int min = s, temp;
+
+	for(int i=s; i<e; i++){
+		if( a[i] < a[min] )
+			min = i;
+	}
+
+	temp = a[min];
+	a[min] = a[s];
+	a[s] = temp;
+
+	SelectionSort(a, s+1, e);
 }
 int main(){
-	int n;
+	int n, a[100];
+	cout<<"Enter array size = ";
 	cin>>n;
-	int a[100];
+	cout<<"Enter " <<n <<" elements in array : \n";
 	for(int i=0;i<n;i++) cin>>a[i];
-	SelectionSort(a,n,0);
+
+	SelectionSort(a,0,n);
+
+	cout<<"Sorted Array = ";
 	for(int i=0;i<n;i++) cout<<a[i]<<" ";
+
 	return 0;
 }
